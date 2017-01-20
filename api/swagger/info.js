ea8a22a408e0b205f2017990415a18436c33d270
zacharyrote <zachary.rote@gmail.com>
2017-01-20 16:09:20 -0500

    change info.js with right name

--- api/swagger/info.js
+++ api/swagger/info.js
@@ -2,7 +2,7 @@
 
 module.exports = {
    info: {
-     title: "Message Delivery Network",
+     title: "Location Service",
      description: "This service is responsible for delivering messages to / from applications",
      version: "1.0.0"
    }
