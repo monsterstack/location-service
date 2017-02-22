9aae80e0dc7da53866e8e92512f2ec9efe3c3349
zacharyrote <zachary.rote@gmail.com>
2017-02-22 12:44:47 -0500

    location-service

--- cluster.js
+++ cluster.js
@@ -34,6 +34,9 @@ const main = () => {
 
   cluster.onProxyReady((proxy) => {
     console.log("Yeah.. the proxy is bound");
+    setInterval(() => {
+      cluster.reannounce(config);
+    }, 2*60*1000);
   });
 }
 
